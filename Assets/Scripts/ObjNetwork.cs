99f25e90986156b5e6337ca6dc31444a4a3461a1
AndrBy1 <abyi@ucsc.edu>
2025-02-15 14:57:14 -0800

    updated branch, improved weapon syncing

--- Assets/Scripts/ObjNetwork.cs
+++ Assets/Scripts/ObjNetwork.cs
@@ -12,21 +12,21 @@ public class ObjNetwork : NetworkBehaviour
     int indexNum = 0;
     public List<GameObject> models; //This is assigned in the unity engine. MAKE SURE TO DRAG THE PREFAB MODEL. AVOID USING THE DROPDOWN LIST.  
     private GameObject model;
+    private bool weaponSwaped = false;
     void Start()
     {
-        // Instantiate the object at the specified position and rotation
-        if(!IsOwner){}
-        model = Instantiate(models[0], netObjpos.Value, netObjrot.Value);
+        // Instantiate the visual model at the specified position and rotation for other players to see
+        if(!IsOwner){model = Instantiate(models[0], netObjpos.Value, netObjrot.Value);}
     }
     void Update()
     {
         if(IsOwner){
             for(int i = 0; i < transform.childCount; i++){ //checks if all the children are active
                 if(transform.GetChild(i).gameObject.activeSelf){
-                    if(i != indexNum){
-                        Destroy(model);
+                    if(i != indexNum){ //If the player changes weapon
+                        Destroy(model); 
                         indexNum = i;
-                        model = Instantiate(models[indexNum], netObjpos.Value, netObjrot.Value);
+                        weaponSwaped = true;
                     }
                     break;
                 }
@@ -35,8 +35,12 @@ public class ObjNetwork : NetworkBehaviour
             netObjrot.Value = transform.GetChild(indexNum).rotation;
             Debug.Log("obj position: " + model.transform.position);
         }else{
+            if(weaponSwaped){ 
+                model = Instantiate(models[indexNum], netObjpos.Value, netObjrot.Value); 
+            }
             model.transform.position = netObjpos.Value;
             model.transform.rotation = netObjrot.Value;
+            
             Debug.Log("model position: " + netObjpos.Value);
         }
         
